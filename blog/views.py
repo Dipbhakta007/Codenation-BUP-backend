b6aec71b7cf2a205f89dc4caee90db67a923dd1d
Dip Bhakta <dipbhakta007@Dips-MacBook-Pro.local>
2024-03-02 11:58:38 +0600

    add redis cache

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,22 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
 from blog.models import Post
 from rest_framework import serializers
 from rest_framework import generics
 from rest_framework import permissions
 from blog import serializers
 
+
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @method_decorator(cache_page(60 * 60 * 2))
+    @method_decorator(vary_on_headers("Authorization"))
+    def get(self, *args, **kwargs):
+        return super().get(*args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
